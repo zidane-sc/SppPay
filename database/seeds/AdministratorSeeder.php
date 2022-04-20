033acc9a4a1ca57ac4e2573053cefc0ab149a25a
zidane-sc <zidankren02@gmail.com>
2022-04-20 22:40:36 +0700

    fix seeder

--- database/seeds/AdministratorSeeder.php
+++ database/seeds/AdministratorSeeder.php
@@ -17,21 +17,23 @@ class AdministratorSeeder extends Seeder
         $administrator->username = "administrator";
         $administrator->name = "Site Administrator";
         $administrator->email = "administrator@SppPay.com";
-        $administrator->roles = "Admin";
+        $administrator->roles = "ADMIN";
         $administrator->password = Hash::make("SppPay");
         $administrator->phone = "082190239674";
         $administrator->address = "Kaliabang Tengah, Bekasi, Jawa Barat";
+        $administrator->status = 'ACTIVE';
         $administrator->save();
         $administrator->assignRole('admin');
-
+        
         $administrator2 = new User();
         $administrator2->username = "staff";
         $administrator2->name = "Site Staff";
         $administrator2->email = "staff@SppPay.com";
-        $administrator2->roles = "Staff";
+        $administrator2->roles = "STAFF";
         $administrator2->password = Hash::make("SppPay");
         $administrator2->phone = "082190239674";
         $administrator2->address = "Kaliabang Tengah, Bekasi, Jawa Barat";
+        $administrator2->status = 'ACTIVE';
         $administrator2->save();
         $administrator2->assignRole('staff');
 
@@ -39,10 +41,11 @@ class AdministratorSeeder extends Seeder
         $administrator3->username = "guru";
         $administrator3->name = "Site Guru";
         $administrator3->email = "guru@SppPay.com";
-        $administrator3->roles = "Guru";
+        $administrator3->roles = "GURU";
         $administrator3->password = Hash::make("SppPay");
         $administrator3->phone = "082190239674";
         $administrator3->address = "Kaliabang Tengah, Bekasi, Jawa Barat";
+        $administrator3->status = 'ACTIVE';
         $administrator3->save();
         $administrator3->assignRole('guru');
 
